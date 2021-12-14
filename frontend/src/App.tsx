c09081921cfb651a2398eb219591c44d9c05a9b2
Caio R. Vieira <caio.robs98@gmail.com>
2021-12-14 20:51:42 -0300

    hello world

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      ta funfando
+      Hello World
       {users.map((user) => (
         <User key={user.email} user={user} />
       ))}
